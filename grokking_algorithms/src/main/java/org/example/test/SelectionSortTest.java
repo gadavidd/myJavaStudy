446a40bc4dd81b87d5240f331d91351bfc4ecbd9
gadavid <34630648+gadavidd@users.noreply.github.com>
2024-07-20 15:50:29 -0300

    Test

--- grokking_algorithms/src/main/java/org/example/test/SelectionSortTest.java
+++ grokking_algorithms/src/main/java/org/example/test/SelectionSortTest.java
@@ -22,7 +22,7 @@ public class SelectionSortTest {
     @Test
     public void testSelection2Sort1() {
         SelectionSort2 selectionSort2 = new SelectionSort2();
-        int[] arr = {5, 3, 6, 2, 10};
+        int[] arr = {5, 3, 6, 2, 10};//change
 
         System.out.println(Arrays.toString(selectionSort2.selectionSort(arr))); // [2, 3, 5, 6, 10]
     }
